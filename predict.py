e7ec871a34f1d31193c1ee42bb7757eec83970b3
Shiou-Ju <75402531+Shiou-Ju@users.noreply.github.com>
2022-07-09 22:18:45 +0800

    feat: workable scripts to train

--- predict.py
+++ predict.py
@@ -4,6 +4,7 @@
 import sys
 import os
 import numpy as np
+from sklearn.preprocessing import LabelEncoder
 from tensorflow.keras import models
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.preprocessing.image import load_img
@@ -15,6 +16,35 @@ digits_in_img = 4
 model = None
 np.set_printoptions(suppress=True, linewidth=150, precision=9,
                     formatter={'float': '{: 0.9f}'.format})
+
+
+# # Mapping characters to integers
+# char_to_num = layers.StringLookup(vocabulary=list(characters), mask_token=None)
+
+# # Mapping integers back to original characters
+# num_to_char = layers.StringLookup(
+#     vocabulary=char_to_num.get_vocabulary(), mask_token=None, invert=True
+# )
+
+
+alphabet = "abcdefghijklmnopqrstuvwxyz0123456789"
+
+
+def convert_indices_to_alpha_numeric(data):
+    #Creates a dict, that maps to every char of alphabet an unique int based on position
+    # char_to_int = dict((c, i) for i, c in enumerate(alphabet))
+    encoded_data = []
+    char_to_int = list(alphabet)
+    #Replaces every char in data with the mapped int
+    # encoded_data.append([char_to_int[char] for char in data])
+    print(char_to_int)
+    # temp = [list(char_to_int.keys()).index(i) for i in data]
+    temp = [char_to_int[i] for i in data]
+    # print(temp)
+    # encoded_data.append()
+    print(temp)  # Prints the int encoded array
+    return temp
+
 
 
 def split_digits_in_img(img_array):
@@ -37,6 +67,8 @@ img_array = img_to_array(img)
 img_rows, img_cols, _ = img_array.shape
 x_list = split_digits_in_img(img_array)
 
+# label_encoder = LabelEncoder()
+
 varification_code = list()
 for i in range(digits_in_img):
     confidences = model.predict(np.array([x_list[i]]), verbose=0)
@@ -45,7 +77,13 @@ for i in range(digits_in_img):
     # https://stackoverflow.com/questions/68836551/keras-attributeerror-sequential-object-has-no-attribute-predict-classes
     predict_x = model.predict(np.array([x_list[i]]), verbose=0)
     result_class = np.argmax(predict_x, axis=1)
+    # print(result_class)
+    # converted = convert_indices_to_alpha_numeric(result_class[0])
     varification_code.append(result_class[0])
+    # pridct_print = label_encoder.inverse_transform(varification_code)
+    # print(pridct_print)
     print('Digit {0}: Confidence=> {1}    Predict=> {2}'.format(
         i + 1, np.squeeze(confidences), np.squeeze(result_class)))
-print('Predicted varification code:', varification_code)
+
+converted = convert_indices_to_alpha_numeric(varification_code)
+print('Predicted varification code:', converted)
--- train.py
+++ train.py
@@ -14,7 +14,7 @@ from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.preprocessing.image import load_img
 
 
-epochs = 10  # 訓練的次數
+epochs = 100  # 訓練的次數
 img_rows = None  # 驗證碼影像檔的高
 img_cols = None  # 驗證碼影像檔的寬
 # TODO: migrate to English letters & numbers
@@ -26,12 +26,49 @@ y_train = list()  # 存訓練用驗證碼內容影像檔array代表的正確內�
 x_test = list()  # 存測試用驗證碼內容影像檔的array
 y_test = list()  # 存測試用驗證碼內容影像檔array代表的正確內容
 
+# # Mapping characters to integers
+# char_to_num = layers.StringLookup(vocabulary=list(characters), mask_token=None)
+
+# # Mapping integers back to original characters
+# num_to_char = layers.StringLookup(
+#     vocabulary=char_to_num.get_vocabulary(), mask_token=None, invert=True
+# )
+
+# Is the alphabet of all possible chars you want to convert
+alphabet = "abcdefghijklmnopqrstuvwxyz0123456789"
+
+
+def convert_alpha_numeric_to_indices(data):
+    #Creates a dict, that maps to every char of alphabet an unique int based on position
+    char_to_int = dict((c, i) for i, c in enumerate(alphabet))
+    encoded_data = []
+    #Replaces every char in data with the mapped int
+    # encoded_data.append([char_to_int[char] for char in data])
+    temp = [char_to_int[char] for char in data]
+    # print(temp)
+    # encoded_data.append()
+    print(temp)  # Prints the int encoded array
+    return temp
+
+    #This part now replaces the int by an one-hot array with size alphabet
+    # one_hot = []
+    # for value in encoded_data:
+    #     #At first, the whole array is initialized with 0
+    #     letter = [0 for _ in range(len(alphabet))]
+    #     #Only at the number of the int, 1 is written
+    #     letter[value] = 1
+    #     one_hot.append(letter)
+    # return one_hot
+
+
 
 def split_digits_in_img(img_array, x_list, y_list):
     for i in range(digits_in_img):
         step = img_cols // digits_in_img
         x_list.append(img_array[:, i * step:(i + 1) * step] / 255)
         y_list.append(img_filename[i])
+        print(y_list)
+        print(img_filename)
 
 
 img_filenames = os.listdir('training')
@@ -50,8 +87,10 @@ for img_filename in img_filenames:
 # 1-1. ValueError: invalid literal for int() with base 10: 'a'
 # y_list = keras.utils.to_categorical(y_list, num_classes=36)
 
-label_encoder = LabelEncoder()
-vec = label_encoder.fit_transform(y_list)
+# label_encoder = LabelEncoder()
+# vec = label_encoder.fit_transform(y_list)
+
+encoded_data = convert_alpha_numeric_to_indices(y_list)
 
 # 1-2. ValueError: Shapes (None, 36) and (None, 10) are incompatible
 # y_list = keras.utils.to_categorical(vec, num_classes=36)
@@ -60,7 +99,7 @@ vec = label_encoder.fit_transform(y_list)
 # 1-3. IndexError: index 10 is out of bounds for axis 1 with size 10
 # y_list = keras.utils.to_categorical(vec, num_classes=10)
 
-y_list = to_categorical(vec, num_classes=36)
+y_list = to_categorical(encoded_data, num_classes=36)
 
 x_train, x_test, y_train, y_test = train_test_split(x_list, y_list)
 
